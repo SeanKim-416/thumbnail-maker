87bb148a0b5ee755aa2db0478740bbba949c784d
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-12-02 17:35:05 +0900

    test: html2canvas 테스트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import html2canvas from 'html2canvas';
+
 function App() {
-  return <div className="App"></div>;
+  const handleClick = async () => {
+    const target = document.querySelector('.target');
+
+    const a = await html2canvas(target);
+    const link = a.toDataURL('image/png');
+    const image = document.createElement('img');
+    image.src = link;
+    document.querySelector('.이미지').appendChild(image);
+  };
+
+  return (
+    <div className="App">
+      <div style={{ width: '500px', height: '400px', backgroundColor: 'red' }} className="target">
+        안녕
+      </div>
+      <button onClick={handleClick}>테스트</button>
+      <div className="이미지"></div>
+    </div>
+  );
 }
 
 export default App;
