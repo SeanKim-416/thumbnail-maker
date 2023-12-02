574a523606b025957db02d8ace42fe88f38fb696
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-12-02 21:39:31 +0900

    add: 버튼 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import html2canvas from 'html2canvas';
+import Button from './Components/atoms/Button';
 
 function App() {
-  const handleClick = async () => {
-    const target = document.querySelector('.target');
-
-    const a = await html2canvas(target);
-    const link = a.toDataURL('image/png');
-    const image = document.createElement('img');
-    image.src = link;
-    document.querySelector('.이미지').appendChild(image);
-  };
-
   return (
     <div className="App">
-      <div style={{ width: '500px', height: '400px', backgroundColor: 'red' }} className="target">
-        안녕
-      </div>
-      <button onClick={handleClick}>테스트</button>
-      <div className="이미지"></div>
+      <>
+        <Button>버튼임</Button>
+      </>
     </div>
   );
 }
 
 export default App;
+
+// const handleClick = async () => {
+//   const target = document.querySelector('.target');
+
+//   const a = await html2canvas(target);
+//   const link = a.toDataURL('image/png');
+//   const image = document.createElement('img');
+//   image.src = link;
+//   document.querySelector('.이미지').appendChild(image);
+// };
+
+{
+  /* <div style={{ width: '500px', height: '400px', backgroundColor: 'red' }} className="target">
+        안녕
+      </div>
+      <button onClick={handleClick}>테스트</button>
+      <div className="이미지"></div> */
+}
